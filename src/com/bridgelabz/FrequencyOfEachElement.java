da2740490bc946cae865a2f82f5c72b443fcc173
akashp69 <akashpakhare499@gmail.com>
2022-11-25 23:45:26 +0530

    Added: Program For To Find the frequency of each element in the Array

---
+++ src/com/bridgelabz/FrequencyOfEachElement.java
@@ -0,0 +1,19 @@
+package com.bridgelabz;
+
+public class FrequencyOfEachElement {
+
+    public static void main(String[] args) {
+
+        int[] array = new int[5];
+        array[0]=10;
+        array[1]=25;
+        array[2]=45;
+        array[3]=15;
+        array[4]=100;
+
+        for(int i=0;i< array.length;i++){
+            System.out.println(array[i]);
+        }
+
+    }
+}
