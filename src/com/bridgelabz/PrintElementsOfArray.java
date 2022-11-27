432f484dad778eecab03a9a812a8d55410820c95
akashp69 <108648856+akashp69@users.noreply.github.com>
2022-11-28 02:11:38 +0530

    Update PrintElementsOfArray.java

--- src/com/bridgelabz/PrintElementsOfArray.java
+++ src/com/bridgelabz/PrintElementsOfArray.java
@@ -2,7 +2,14 @@ package com.bridgelabz;
 
 public class PrintElementsOfArray {
     public static void main(String[] args) {
+     /**
+    *This Method is Created For Print The  Elements of Array
+    */
         int[] array = new int[]{10,20,30,40,50};
+        
+    /**
+    *Used For Loop For Print The Elements Present In an Array
+    */
 
 
         for(int i=0;i< array.length;i++){
