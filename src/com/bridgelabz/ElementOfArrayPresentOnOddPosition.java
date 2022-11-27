fb87839458ee75d21a21c593ad8c8f7223583789
akashp69 <108648856+akashp69@users.noreply.github.com>
2022-11-28 02:05:16 +0530

    Update ElementOfArrayPresentOnOddPosition.java

--- src/com/bridgelabz/ElementOfArrayPresentOnOddPosition.java
+++ src/com/bridgelabz/ElementOfArrayPresentOnOddPosition.java
@@ -1,8 +1,15 @@
 package com.bridgelabz;
 
 public class ElementOfArrayPresentOnOddPosition {
+    /**
+    *This Method is Created For Find The  Elements of Array On Odd Position
+    */
     public static void main(String[] args) {
+    
         int array[] = new int[]{10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
+    /**
+    *Using For loop to get Elements of Array On Odd Position
+    */
 
         for (int i = 0; i < array.length; i = i + 2) {
             System.out.println(array[i]);
