e9f815c5ab5471df39cd7dacf4828bb86608e95b
akashp69 <108648856+akashp69@users.noreply.github.com>
2022-11-28 02:14:05 +0530

    Update PrintElementsOfArrayInReverseOrder.java

--- src/com/bridgelabz/PrintElementsOfArrayInReverseOrder.java
+++ src/com/bridgelabz/PrintElementsOfArrayInReverseOrder.java
@@ -1,8 +1,15 @@
 package com.bridgelabz;
 
 public class PrintElementsOfArrayInReverseOrder {
+    /**
+    *This Method is Created For arrange The  Elements of Array in Reverse Order
+    */
     public static void main(String[] args) {
         int array[]=new int[]{10,20,30,40,50,60,70,80,90,100};
+      
+    /**
+    *Used For Loop For Print The Elements in Array In Reverse Order
+    */
 
         for(int i=9;array.length-1>=0;i--){
             System.out.println(array[i]);
