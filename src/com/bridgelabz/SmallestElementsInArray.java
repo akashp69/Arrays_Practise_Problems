52407772dc653fc531e00419fd19b387c69196eb
akashp69 <108648856+akashp69@users.noreply.github.com>
2022-11-28 02:15:34 +0530

    Update SmallestElementsInArray.java

--- src/com/bridgelabz/SmallestElementsInArray.java
+++ src/com/bridgelabz/SmallestElementsInArray.java
@@ -2,6 +2,9 @@ package com.bridgelabz;
 
 public class SmallestElementsInArray {
     public static void main(String[] args) {
+    /**
+    *This Method is Created For Find The Smallest Elements of Array 
+    */
 
         int array[] = {150, 450,490, 100, 350 };
         int a, b, c, d, e;
@@ -10,6 +13,10 @@ public class SmallestElementsInArray {
         c = array[2];
         d = array[3];
         e = array[4];
+        
+    /**
+    *Using if-else for Find The smallest Elements Of Array 
+    */
 
 
         if (a< b && a < c && a < d && a < e) {
