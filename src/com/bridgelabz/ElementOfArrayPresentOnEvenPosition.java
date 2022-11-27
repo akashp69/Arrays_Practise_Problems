0043a56f97523f8ae981dfc822573429de65bd92
akashp69 <108648856+akashp69@users.noreply.github.com>
2022-11-28 02:03:12 +0530

    Update ElementOfArrayPresentOnEvenPosition.java

--- src/com/bridgelabz/ElementOfArrayPresentOnEvenPosition.java
+++ src/com/bridgelabz/ElementOfArrayPresentOnEvenPosition.java
@@ -1,8 +1,14 @@
 package com.bridgelabz;
 
 public class ElementOfArrayPresentOnEvenPosition {
+    /**
+    *This Method Created For Print The Elements Present on Even Position
+    */
     public static void main(String[] args) {
         int array[]=new int[]{10,20,30,40,50,60,70,80,90,100};
+    /**
+    *Using For loop to get Elements of Array On Odd Position
+    */
 
         for(int i=1;i< array.length;i=i+2){
             System.out.println(array[i]);
